82297c50b3b9a87db460a192e12b5b9cfbaaa024
Ammar <outseccoc@hotmail.com>
2026-05-12 01:20:42 +0300

    api 2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
       allowedHeaders: 'Content-Type, Authorization',
     },
   });
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
